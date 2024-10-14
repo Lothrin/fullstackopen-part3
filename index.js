2ac3f8dc109395c72c8bb106185a35fe00a3e21a
Lothrin <m.erkam.arafat@gmail.com>
2024-10-14 15:02:10 +0200

    3.8*: Phonebook backend step 8

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
+morgan.token('req-body', (req) => JSON.stringify(req.body));
+
 
 let persons = [
     { 
