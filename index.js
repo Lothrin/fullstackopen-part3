69d37839c25c4b429f42d8f2d401be0e41ccbb64
Lothrin <m.erkam.arafat@gmail.com>
2024-10-17 13:18:14 +0200

    3.9 Phonebook backend step 9
    -Installed CORS
    
    Additionally:
    -Copied the phonebook frontend files from PART2 to an external local folder.
    -Connected this backend to the frontend local files from part2 without making changes to PART2 repository.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
 morgan.token('req-body', (req) => JSON.stringify(req.body));
@@ -91,6 +93,6 @@ const generateId = () => {
     response.json(person)
   })
  
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
