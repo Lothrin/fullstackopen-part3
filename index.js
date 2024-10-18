082efc1d97b119a5f2f02648737db805e1f81433
Lothrin <m.erkam.arafat@gmail.com>
2024-10-18 20:58:04 +0200

    3.19*: Phonebook database, step 7

--- index.js
+++ index.js
@@ -44,18 +44,18 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
+if (!body.name || !body.number) {
     return response.status(400).json({ error: 'name or number is missing' })
   }
 
-  const personExists = Person.findOne({ name: body.name })
-
-  if(personExists) {
-    Person.findByIdAndUpdate(personExists._id, { number: body.number }, { new: true})
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+  Person.findOne({ name: body.name })
+    .then(personExists => {
+      if (personExists) {
+        Person.findByIdAndUpdate(personExists._id, { number: body.number }, { new: true })
+          .then(updatedPerson => {
+            response.json(updatedPerson);
+          })
+          .catch(error => next(error));
   }else{
   const person = new Person({
     name: body.name,
@@ -65,7 +65,10 @@ app.post('/api/persons', (request, response, next) => {
     response.json(savedPerson)
   })
   .catch(error => next(error))
-}})
+}
+})
+.catch(error => next(error)); 
+});
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
@@ -75,7 +78,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -94,7 +97,9 @@ app.put('/api/persons/:id', (request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
   
     next(error)
   }
@@ -138,3 +143,13 @@ console.log(`Server running on port ${PORT}`)
 //   }
 // ]
 
+
+
+// const personExists = Person.findOne({ name: body.name })
+
+//   if(personExists) {
+//     Person.findByIdAndUpdate(personExists._id, { number: body.number }, { new: true})
+//     .then(updatedPerson => {
+//       response.json(updatedPerson)
+//     })
+//     .catch(error => next(error))
